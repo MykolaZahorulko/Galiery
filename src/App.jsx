6251305d6d2c50499757b4b311ae8c1db1021515
MykolaZahorulko <mykolazahorulko.work@gmail.com>
2023-11-10 20:04:40 +0100

    initial header branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './components/home/Home.jsx';
+import Home from './components/pages/home/Home.jsx';
 import Layout from "./layout/Layout.jsx";
 // import AdminPanel from './components/AdminPanel';
 
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './Layout.module.scss'
-// import Header from '../components/common/header/Header.jsx'
-// import Footer from '../components/common/footer/Footer.jsx'
+import Header from '../components/common/header/Header.jsx'
+import Footer from '../components/common/footer/Footer.jsx'
 const Layout = ({ children }) => {
     return (
         <div className={styles.wrapper}>
-            {/*<Header />*/}
+            <Header />
             <main className={styles.main}>{children}</main>
-            {/*<Footer />*/}
+            <Footer />
         </div>
     )
 }
