ebeccb7324104e8f233d84441255d6ecfe5c3723
MykolaZahorulko <mykolazahorulko.work@gmail.com>
2023-12-07 22:07:58 +0100

    added faq not full page

--- src/components/pages/faq/Faq.jsx
+++ src/components/pages/faq/Faq.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from './Faq.module.scss'
+import items from './FaqItems.js'
 
 const Faq = () => {
+    const [openIndex, setOpenIndex] = useState(null);
+
+    const handleClick = (index) => {
+        setOpenIndex(openIndex === index ? null : index);
+    }
+
     return (
-        <div>Faq</div>
+        <div className={styles.faq}>
+            <div className={`${styles.faq__container} ${styles.container}`}>
+                <h2 className={`${styles.faq__title} ${styles.title}`}>F.A.Q</h2>
+                <div className={styles.faq__body}>
+                    {items.map((item, index) => (
+                        <div key={index} className={`${styles.faq__item} ${openIndex === index ? styles.open : ''}`}>
+                            <div className={styles.faq__item_title} onClick={() => handleClick(index)}>
+                                {item.title}
+                            </div>
+                            {openIndex === index ? (
+                                    <div
+                                        className={styles.faq__item_content}>
+                                        {item.content}
+                                    </div>
+                            ) : null}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 export default Faq
